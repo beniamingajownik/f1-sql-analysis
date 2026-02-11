5d556b1df9c04b3b49f789af809bce1e3394905d
Beniamin <beniamin.gajownik@gmail.com>
2026-02-11 12:26:33 +0100

    Add points correction for 2018 and 2020

--- views/analytics/v_analytics_constructor_standings.sql
+++ views/analytics/v_analytics_constructor_standings.sql
@@ -27,10 +27,23 @@ WITH aggregated_season AS (
 		engine_manufacturer,
 		regulation_era,
 		-- Summing only the points that counted towards the championship title
-		SUM(CASE WHEN counts_to_championship = 1 THEN team_points ELSE 0 END) total_points		
+		SUM(CASE WHEN counts_to_championship = 1 THEN team_points ELSE 0 END) total_points_before_correction		
 	FROM v_constructor_championship_logic
 	GROUP BY year, constructor_name, constructor_id, constructor_nationality, constructor_continent, engine_manufacturer, regulation_era
+),
+
+points_correction AS (
+	SELECT 
+		*,
+		-- Correcting total season points (2020 Racing Point was deducted 15pts. for illegal rear brake system while 2018 Force India went bankrupt)
+		CASE
+			WHEN year = 2020 AND constructor_id = 'racing-point' THEN total_points_before_correction - 15.0
+			WHEN year = 2018 AND constructor_id = 'force-india'	 THEN total_points_before_correction - 59.0
+			ELSE total_points_before_correction
+		END total_points
+	FROM aggregated_season
 )
+
 SELECT
 	*,
 	-- Ranking constructors within each season based on their points tally
@@ -38,4 +51,4 @@ SELECT
 		WHEN year < 1958 THEN 0
 		ELSE RANK() OVER(PARTITION BY year ORDER BY total_points DESC)
 	END season_position
-FROM aggregated_season;
+FROM points_correction;
